4b78c0373ff4bc9421f5d10cc4a1a8e64b3b742e
Gregory Favre <greg@beyondthewall.ch>
2022-05-16 23:36:45 +0200

    db path as param

--- pyradar/cli.py
+++ pyradar/cli.py
@@ -9,7 +9,7 @@ from .pyradar import run
 @click.option('--db', default='/tmp/pyradar/pyradar.db', type=click.Path(), show_default=True, )
 @click.option('--min-speed', default=30.0, type=click.FLOAT, show_default=True)
 @click.option('--min-recording-seconds', default=5.0, type=click.FLOAT, show_default=True)
-def main(video_dir, db_path, min_speed, min_recording_seconds):
+def main(video_dir, db, min_speed, min_recording_seconds):
     """Console script for pyradar."""
     run(video_dir=video_dir, db_path=db_path, min_speed=min_speed, min_recording_seconds=min_recording_seconds)
     # click.echo("See click documentation at https://click.palletsprojects.com/")
